eb68a0dfd804aa9e60cab42f50e21d47788a500e
Simone Di Mario <sdi-mari@e3r5p5.42roma.it>
2023-04-17 19:00:39 +0200

    17 Aprile mod2

---
+++ ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdi-mari <sdi-mari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/17 18:53:50 by sdi-mari          #+#    #+#             */
+/*   Updated: 2023/04/17 19:00:08 by sdi-mari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void
+	*ft_memmove(void *dest, const void *src, size_t len)
+{
+	int	i;
+
+	if (!dest || !src)
+		return (NULL);
+	if (dest > src)
+	{
+		i = (int)len - 1;
+		while (i >= 0)
+		{
+			*(char *)(dest + i) = *(char *)(src + i);
+			--i;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < (int)len)
+		{
+			*(char *)(dest + i) = *(char *)(src + i);
+			++i;
+		}
+	}
+	return (dest);
+}
