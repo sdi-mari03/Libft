aa4fa83d0da201c0fb46569552366365f1ce17f8
Simone Di Mario <sdi-mari@e3r5p5.42roma.it>
2023-04-17 18:52:53 +0200

    17 Aprile mod1

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,11 +6,11 @@
 /*   By: sdi-mari <sdi-mari@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 18:45:48 by sdi-mari          #+#    #+#             */
-/*   Updated: 2023/04/17 18:49:29 by sdi-mari         ###   ########.fr       */
+/*   Updated: 2023/04/17 18:52:00 by sdi-mari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "linft.h"
+#include "libft.h"
 
 void
 	*ft_memcpy(void *dest, const void *src, size_t n)
