5ba0109783dfb35573165e45949829dd3f279db4
Simone Di Mario <sdi-mari@e3r2p3.42roma.it>
2023-05-05 12:21:39 +0200

    5 Maggio

---
+++ ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdi-mari <sdi-mari@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 11:50:45 by sdi-mari          #+#    #+#             */
+/*   Updated: 2023/05/05 12:03:18 by sdi-mari         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = rsc[i];
+			++i;
+		}
+		dst[i] = 0;
+	}
+	while (srcp[i])
+	++i;
+	return (i);
+}
